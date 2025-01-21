3cfeaabb7a9486597c46f7d21825f3efc46c3e5b
hf-sabillon <hf.sabillon@levanterglobal.com>
2025-01-21 14:48:04 -0600

    PropioApp

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using AutoUpdaterDotNET;
+
 namespace PropioApp
 {
     internal static class Program
@@ -11,9 +13,26 @@ namespace PropioApp
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+
+
+
+            // Configurar AutoUpdater
+            AutoUpdater.ApplicationExitEvent += AutoUpdater_ApplicationExitEvent;
+            AutoUpdater.Start("https://tu-servidor.com/actualizaciones/update.xml");
+
+            // Continuar con el inicio de la aplicación
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+
             Application.Run(new PropioTrayForm());
         }
 
+        private static void AutoUpdater_ApplicationExitEvent()
+        {
+            // Lógica para reiniciar la aplicación si es necesario
+            Application.Exit();
+        }
 
     }
 }
